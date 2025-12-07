17485f89586317cac845cfcd4ca267ed0e97d184
Subbulakshmi.K <subbuviswa94.sastra@gmail.com>
2025-12-08 04:02:33 +0530

    AllRequests

--- src/test/java/com/subbu/rest/test/GetCallBDD.java
+++ src/test/java/com/subbu/rest/test/GetCallBDD.java
@@ -1,28 +1,145 @@
 package com.subbu.rest.test;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class GetCallBDD {
 
-	@Test
-	public void test_getCallBDD() {
+//	@Test
+//	public void test_getCallBDD() {
+////		given().
+////		when().
+////		then().
+////		assert()
+//		
 //		given().
 //		when().
+//			get("https://bookstore.toolsqa.com/BookStore/v1/Books").
 //		then().
-//		assert()
-		
-		given().
-		when().
-			get("https://bookstore.toolsqa.com/BookStore/v1/Books").
-		then().
-			assertThat().
-			statusCode(200).
-			and().
-			body("books.isbn",hasSize(8)).
-			and().
-			header("Content-Length",equalTo("4514"));
-	}
+//			assertThat().
+//			statusCode(200).
+//			and().
+//			body("books.isbn",hasSize(8)).
+//			and().
+//			header("Content-Length",equalTo("4514"));
+//	}
+	
+    @BeforeSuite
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void getRequest() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/posts")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("qui est esse", response.jsonPath().getString("title[1]"));
+    }
+    
+    @Test
+    public void getRequestWithQueryParam() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .param("postId", "2")
+                .when()
+                .get("/comments")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("Meghan_Littel@rene.us", response.jsonPath().getString("email[3]"));
+    }
+    
+    private static String requestBody = "{\n" +
+            "  \"title\": \"foo\",\n" +
+            "  \"body\": \"bar\",\n" +
+            "  \"userId\": \"1\" \n}";
+    
+    @Test
+    public void postRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/posts")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(201, response.statusCode());
+        Assert.assertEquals("foo", response.jsonPath().getString("title"));
+        Assert.assertEquals("bar", response.jsonPath().getString("body"));
+        Assert.assertEquals("1", response.jsonPath().getString("userId"));
+        Assert.assertEquals("101", response.jsonPath().getString("id"));
+    }
+    
+    private static String putRequestBody = "{\n" +
+            "  \"title\": \"foo\",\n" +
+            "  \"body\": \"baz\",\n" +
+            "  \"userId\": \"1\",\n" +
+            "  \"id\": \"1\" \n}";
+    
+    @Test
+    public void putRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(putRequestBody)
+                .when()
+                .put("/posts/1")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("foo", response.jsonPath().getString("title"));
+        Assert.assertEquals("baz", response.jsonPath().getString("body"));
+        Assert.assertEquals("1", response.jsonPath().getString("userId"));
+        Assert.assertEquals("1", response.jsonPath().getString("id"));
+    }
+    
+    private static String patchRequestBody = "{\n" +
+            "  \"title\": \"bax\" \n}";
+    
+    @Test
+    public void patchRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(patchRequestBody)
+                .when()
+                .patch("/posts/1")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("bax", response.jsonPath().getString("title"));
+        Assert.assertEquals("1", response.jsonPath().getString("userId"));
+        Assert.assertEquals("1", response.jsonPath().getString("id"));
+    }
+    
+    @Test
+    public void deleteRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .when()
+                .delete("/posts/1")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+    }
 }
